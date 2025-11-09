edddb1af05f9f3f6dfd848d344be3d98037600ba
Ankit Kaurav <ankitkaurav832@gmail.com>
2025-11-09 22:59:58 +0530

    minor change

--- todo_list_backend/src/main/java/com/ankit/todo_list/security/SecurityConfig.java
+++ todo_list_backend/src/main/java/com/ankit/todo_list/security/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("https://todo-list-project-1-x8tt.onrender.com"));
 
         configuration.setAllowedMethods(List.of("GET", "POST", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
